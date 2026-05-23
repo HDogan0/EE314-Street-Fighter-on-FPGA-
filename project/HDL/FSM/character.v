d6fe35978d8a482d13a2b6bb7b403353f5f60b22
Hasan <121304724+HDogan0@users.noreply.github.com>
2026-05-23 17:00:02 +0300

    Update character.v

--- project/HDL/FSM/character.v
+++ project/HDL/FSM/character.v
@@ -9,8 +9,8 @@ module character(
     input special_hit_flag,                             // special attack flag
     input attack_success,                               // attack collision box overlapped with opponent hurtbox flag
     output reg KO_flag,                                 // knocked-out output flag
-    output reg [2:0] state,                             // state indicator according to localparam below
-    output reg [6:0] frame_counter,                     // frame tick counter output
+    output reg [2:0] current_state,                     // state indicator according to localparam below
+    output reg [6:0] frame_tick,								  // frame tick counter output
     output reg [1:0] remaining_blockings                // remaining block chances
 );
 
@@ -65,177 +65,164 @@ module character(
     always @(*) begin:COMB
         // to avoid additional latches
         next_state = state;
-        KO_flag = 0;
 
-        // if the character is hit by special attack and it is not moving backward it gets knocked-out
-        // no matter the current state is. 
-        if (special_hit_flag && state != s_move_backward) begin 
-            KO_flag = 1;
-            // NOTE THAT, I assigned next_state as idle, we can arrange it later.
-            next_state = s_idle;
-        end 
-
-        else begin 
-
-            case(state) 
-                
-                //
-                //  IDLE STATE IMPLEMENTATION
-                //  
-                s_idle:begin
-                    if(special_attack_trigger) begin 
-                        next_state = s_special_attack;
-                    end
-                    else if(default_attack_trigger) begin 
-                        next_state = s_default_attack;
-                    end
-                    else if(move_forward) begin 
-                        next_state = s_move_forward;
-                    end
-                    else if(move_backward) begin 
-                        next_state = s_move_backward;
-                    end
-                    else begin 
-                        //  default case should be added for safety
-                        next_state = s_idle;
-                    end
+        case(state) 
+            
+            //
+            //  IDLE STATE IMPLEMENTATION
+            //  
+            s_idle:begin
+                if(special_attack_trigger) begin 
+                    next_state = s_special_attack;
                 end
-
-                // 
-                //  MOVE FORWARD STATE IMPLEMENTATION
-                // 
-                s_move_forward:begin 
-                    if(!move_forward) begin
-                        next_state = s_idle;
-                    end
-                    else begin 
-                        next_state = s_move_forward;
-                    end
+                else if(default_attack_trigger) begin 
+                    next_state = s_default_attack;
                 end
-
-                //
-                //  MOVE BACKWARD STATE IMPLEMENTATION
-                //                  
-                s_move_backward:begin
-                    //  if somehow the character is hit while moving back  
-                    if(hit_flag || special_hit_flag) begin
-                        if(remaining_blockings > 0) begin 
-                            next_state = s_blockstun;
-                        end 
-                        else begin 
-                            // remaining_blockings == 0
-                            next_state = s_guard_break;
-                        end
-                    end
-                    else if(!move_backward) begin 
-                        next_state = s_idle;
-                    end
-                    else begin 
-                        next_state = s_move_backward;
-                    end
+                else if(move_forward) begin 
+                    next_state = s_move_forward;
                 end
-
-                //
-                //  DEFAULT ATTACK STATE IMPLEMENTATION
-                //  
-                s_default_attack:begin
-                    //  if character is hit while attacking
-                    if(hit_flag || special_hit_flag) begin 
-                        next_state = s_hitstun;
-                    end
-                    //  successfully attacked with no interruption
-                    else if(frame_counter >= default_attack_frame_number - 1)begin 
-                        next_state = s_idle;
-                    end
-                    //  if character spammed default attack in recovery phase of a successful attack
-                    else if(frame_counter >= (default_attack_startup_frame_number + default_attack_active_frame_number) && default_attack_trigger && attack_success) begin 
-                        next_state = s_special_attack;
-                    end
-
-                    else begin 
-                        next_state = s_default_attack;
-                    end
+                else if(move_backward) begin 
+                    next_state = s_move_backward;
                 end
-
-                //
-                //  SPECIAL ATTACK STATE IMPLEMENTATION
-                //  
-                s_special_attack:begin
-                    //  if character is hit while attacking
-                    if(hit_flag || special_hit_flag) begin 
-                        next_state = s_hitstun;
-                    end
-                    //  successfully attacked with no interruption
-                    else if(frame_counter >= special_attack_frame_number - 1) begin 
-                        next_state = s_idle;
-                    end
-                    else begin 
-                        next_state = s_special_attack;
-                    end
+                else begin 
+                    //  default case should be added for safety
+                    next_state = s_idle;
                 end
+            end
 
-                //
-                //  HITSTUN STATE IMPLEMENTATION
-                //  
-                s_hitstun:begin
-                    //  IF HIT BY DEFAULT ATTACK, OTHERWISE IT IS A KO
-                    if((frame_counter >= hitstun_default_attack_frame_number - 1) && !hit_by_special) begin 
-                        next_state = s_idle;
-                    end
-                    else begin 
-                        next_state = s_hitstun;
-                    end
+            // 
+            //  MOVE FORWARD STATE IMPLEMENTATION
+            // 
+            s_move_forward:begin 
+                if(!move_forward) begin
+                    next_state = s_idle;
                 end
+                else begin 
+                    next_state = s_move_forward;
+                end
+            end
 
-                //
-                //  BLOCKSTUN STATE IMPLEMENTATION
-                //  
-                s_blockstun:begin
-                    //  if hit by default attack
-                    if((frame_counter >= blockstun_default_attack_frame_number - 1) && !hit_by_special) begin 
-                        next_state = s_idle;
-                    end
-                    //  if hit by special attack
-                    else if((frame_counter >= blockstun_special_attack_frame_number - 1) && hit_by_special) begin 
-                        next_state = s_idle;
-                    end
-                    else begin 
+            //
+            //  MOVE BACKWARD STATE IMPLEMENTATION
+            //                  
+            s_move_backward:begin
+                //  if somehow the character is hit while moving back  
+                if(hit_flag || special_hit_flag) begin
+                    if(remaining_blockings > 0) begin 
                         next_state = s_blockstun;
-                    end
-                end
-
-                //
-                //  GUARD BREAK STATE IMPLEMENTATION
-                //  
-                s_guard_break:begin
-                    //  if hit by default attack 
-                    if((frame_counter >= guard_break_default_attack_frame_number - 1) && !hit_by_special) begin 
-                        next_state = s_idle;
-                    end
-                    //  if hit by special attack
-                    else if((frame_counter >= guard_break_special_attack_frame_number - 1) && hit_by_special) begin 
-                        next_state = s_idle;
-                    end
-
+                    end 
                     else begin 
+                        // remaining_blockings == 0
                         next_state = s_guard_break;
                     end
                 end
+                else if(!move_backward) begin 
+                    next_state = s_idle;
+                end
+                else begin 
+                    next_state = s_move_backward;
+                end
+            end
 
-            endcase
-        end
+            //
+            //  DEFAULT ATTACK STATE IMPLEMENTATION
+            //  
+            s_default_attack:begin
+                //  if character is hit while attacking
+                if(hit_flag || special_hit_flag) begin 
+                    next_state = s_hitstun;
+                end
+                //  successfully attacked with no interruption
+                else if(frame_counter >= default_attack_frame_number - 1)begin 
+                    next_state = s_idle;
+                end
+                //  if character spammed default attack in recovery phase of a successful attack
+                else if(frame_counter >= (default_attack_startup_frame_number + default_attack_active_frame_number) && default_attack_trigger && attack_success) begin 
+                    next_state = s_special_attack;
+                end
+
+                else begin 
+                    next_state = s_default_attack;
+                end
+            end
+
+            //
+            //  SPECIAL ATTACK STATE IMPLEMENTATION
+            //  
+            s_special_attack:begin
+                //  if character is hit while attacking
+                if(hit_flag || special_hit_flag) begin 
+                    next_state = s_hitstun;
+                end
+                //  successfully attacked with no interruption
+                else if(frame_counter >= special_attack_frame_number - 1) begin 
+                    next_state = s_idle;
+                end
+                else begin 
+                    next_state = s_special_attack;
+                end
+            end
+
+            //
+            //  HITSTUN STATE IMPLEMENTATION
+            //  
+            s_hitstun:begin
+                //  IF HIT BY DEFAULT ATTACK, OTHERWISE IT IS A KO
+                if((frame_counter >= hitstun_default_attack_frame_number - 1) && !hit_by_special) begin 
+                    next_state = s_idle;
+                end
+                else begin 
+                    next_state = s_hitstun;
+                end
+            end
+
+            //
+            //  BLOCKSTUN STATE IMPLEMENTATION
+            //  
+            s_blockstun:begin
+                //  if hit by default attack
+                if((frame_counter >= blockstun_default_attack_frame_number - 1) && !hit_by_special) begin 
+                    next_state = s_idle;
+                end
+                //  if hit by special attack
+                else if((frame_counter >= blockstun_special_attack_frame_number - 1) && hit_by_special) begin 
+                    next_state = s_idle;
+                end
+                else begin 
+                    next_state = s_blockstun;
+                end
+            end
+
+            //
+            //  GUARD BREAK STATE IMPLEMENTATION
+            //  
+            s_guard_break:begin
+                //  if hit by default attack 
+                if((frame_counter >= guard_break_default_attack_frame_number - 1) && !hit_by_special) begin 
+                    next_state = s_idle;
+                end
+                //  if hit by special attack
+                else if((frame_counter >= guard_break_special_attack_frame_number - 1) && hit_by_special) begin 
+                    next_state = s_idle;
+                end
+
+                else begin 
+                    next_state = s_guard_break;
+                end
+            end
+
+        endcase
     end
 
-    always @(posedge game_clk || negedge rst) begin:SEQ
+    always @(posedge game_clk or posedge rst) begin:SEQ
         if(rst) begin 
             state <= s_idle;
             frame_counter <= 0;
             remaining_blockings <= 3;
             hit_by_special <= 0;
         end
-        else begin 
-            state <= next_state;
-
+        else begin
+		  state <= next_state;
             //  remainin blockings logic
             if(state==s_move_backward && (hit_flag || special_hit_flag) && remaining_blockings >0) begin 
                 remaining_blockings <= remaining_blockings - 1;
@@ -266,4 +253,15 @@ module character(
         end
     end
 
+	always @(*) begin:OUT
+			KO_flag = 0;
+		  // if the character is hit by special attack and it is not moving backward it gets knocked-out
+        // no matter the current state is. 
+        if (special_hit_flag && state != s_move_backward) begin 
+            KO_flag = 1;
+        end 
+		current_state = state;
+		frame_tick = frame_counter;
+	end
+	 
 endmodule
