9f2c36b11ba16b3d415c4a1f841f90f80545733d
ahmett04 <165032788+ahmett04@users.noreply.github.com>
2026-06-04 12:09:10 +0300

    Add files via upload

---
+++ project/HDL/FSM/hitbox.v
@@ -0,0 +1,62 @@
+`include "states.vh"
+module hitbox (
+    // ============================================================
+    //                    SİSTEM SİNYALLERİ
+    // ============================================================
+    input  wire  [2:0]  player_state,
+
+    // player karakterinin sol-üst köşesinin X koordinatı (0..639).
+    input  wire [9:0]  player_x,
+    // ============================================================
+    //  Diğer modüle GİDEN HİTBOX/HURTBOX KOORDİNATLARI (Debug görüntü)
+    // ============================================================
+    // Tüm kutular: sol-üst köşe (x,y) + genişlik w + yükseklik h.
+    // 10-bit, hepsi pozitif değer.
+    
+    // player ana gövde hurtbox: her zaman aktif, sabit boyut
+    output wire [9:0]  player_hurtbox_body_x,
+    output wire [9:0]  player_hurtbox_body_y, 
+    output wire [9:0]  player_hurtbox_body_w,
+    output wire [9:0]  player_hurtbox_body_h,
+
+    // player uzantı hurtbox: sadece recovery state'lerinde aktif (whiff-punish için)
+    // ayak/kol kısmı, gövdeyi büyütmek yerine ayrı bir kutu olarak eklenir. ama bunu dışarıya ekstra bir output olarak değil hit detectora kontrol vererek yap.
+    // yani hitbox sonradan hurtboxa dönüşüyor ya, onu burada ayrı bir external hurtbox olarak tanımlamaya gerek yok, top modül frame ticke bakıp hangi statede 
+    // olduğunu bulsun ve ona göre desin ki tamam artık recovery statede o zaman hitboxu da hurtbox olarak alıyım kesişimler ona göre kontrol edilsin.
+
+    // player hitbox: sadece s_default_attack / s_special_attack state'lerinde aktif.
+    output wire [9:0]  player_hitbox_x,
+    output wire [9:0]  player_hitbox_y,
+    output wire [9:0]  player_hitbox_w,
+    output wire [9:0]  player_hitbox_h
+);
+
+//reg ve diğer parametre tanımları
+localparam SCREEN_W = 640;
+localparam SCREEN_H = 480;
+localparam PLAYER_W = 64;
+localparam PLAYER_H = 240;
+localparam PLAYER_Y_OFFSET = 30;          // playerin ayakları ekranın en altından ne kadar yukarıda olacak...
+
+localparam HITBOX_H = 80;                   // hitbox yüksekliği
+localparam HITBOX_HEIGTH_DIFFERENCE = 140;     // basic attack ile sp attack yaparken hitboxların y eksenindeki konumları farklı. specialın basicten ne kadar yüksekte olduğunu gösteriyor bu
+
+localparam HITBOX_W_BASIC = 64;            // basic atak yaparken hitbox genişliği
+localparam HITBOX_W_SPECIAL = 64;            // sp atak yaparken hitbox genişliği
+
+//DİKKAT4: VGA koordinat sisteminde Y üstten aşağıya artar. Ondan SCREEN_H (yani 480) - ... yazıldı HITBOX_Y_BASIC
+localparam HITBOX_Y_BASIC = SCREEN_H - HITBOX_H - PLAYER_Y_OFFSET;            // basic atak yaparken hitbox sol üst köşe y'si
+localparam HITBOX_Y_SPECIAL = SCREEN_H - HITBOX_H - PLAYER_Y_OFFSET - HITBOX_HEIGTH_DIFFERENCE;     
+
+// ============= HITBOX TANIMLARI ===================
+assign player_hitbox_x = player_x + PLAYER_W / 2; // basic veya sp. atak yaparken hitbox sol üst köşe x kordinatı, playerin konumuna göre gelir
+
+assign player_hitbox_y = (player_state == `s_special_attack) ? HITBOX_Y_SPECIAL :
+                         (player_state == `s_default_attack) ? HITBOX_Y_BASIC   : 0;
+                         
+assign player_hitbox_w = (player_state == `s_special_attack) ? HITBOX_W_SPECIAL : 
+                         (player_state == `s_default_attack) ? HITBOX_W_BASIC;  : 0;
+
+assign player_hitbox_h = HITBOX_H;
+
+endmodule
---
+++ project/HDL/FSM/hurtbox.v
@@ -0,0 +1,50 @@
+`include "states.vh"
+module hurtbox (
+    // SORUN OLMA İHTİMALİ OLAN YERLERDE "DİKKAT" NOTLARI VAR. özellikle DİKKAT6'ya bir daha bakın açıklamaya
+    // HURTBOX'U ŞÖYLE TASARLADIK. BU MODÜLDEKİ HURTBOX SADECE KİŞİNİN KENDİ BODYSİ, YANİ RECOVER OLURKEN HITBOX HANI HURTBOXA DÖNÜŞÜYOR YA, O KONTROL BURADA DEĞİL, ONU TOP MODÜLDE AYARLAMALISINIZ.
+    // KULLANICININ HANGI STATEDE OLDUĞUNA BAKARAK. EĞER ŞU STATEDE İSE HITBOXU HURTBOXA DÖNÜŞTÜR O ŞEKİLDE AL olacak mantık.
+    // ============================================================
+    //                    SİSTEM SİNYALLERİ
+    // ============================================================
+    input  wire        player_left,
+    input  wire        player_right,
+    input  wire        player_hitbox_active,
+    input  wire  [3:0]  player_state,
+
+    // player karakterinin sol-üst köşesinin X koordinatı (0..639).
+    output reg  [9:0]  player_x,   
+
+    // ============================================================
+    //  Diğer modüle GİDEN HİTBOX/HURTBOX KOORDİNATLARI (Debug görüntü)
+    // ============================================================
+    // Tüm kutular: sol-üst köşe (x,y) + genişlik w + yükseklik h.
+    // 10-bit, hepsi pozitif değer.
+    
+    // player hurtbox: state'e bağlı olarak dinamik boyut.
+    // Idle/yürüme'de normal, recovery'de genişlemiş (whiff-punish için).
+    output wire [9:0]  player_hurtbox_x,
+    output wire [9:0]  player_hurtbox_y, 
+    output wire [9:0]  player_hurtbox_w,
+    output wire [9:0]  player_hurtbox_h,
+);
+
+// reg ve diğer parametre tanımları
+localparam SCREEN_W = 640;
+localparam SCREEN_H = 480;
+localparam PLAYER_W = 64;
+localparam PLAYER_H = 240;
+localparam PLAYER_Y_OFFSET = 30;          // playerin ayakları ekranın en altından ne kadar yukarıda olacak...
+
+localparam HURTBOX_W = 64;           
+localparam HURTBOX_H = 80;                 
+
+// ============= HURTBOX TANIMLARI ===================
+assign player_hurtbox_x = player_x;
+
+assign player_hurtbox_y = SCREEN_H - PLAYER_Y_OFFSET - PLAYER_H;  // tabandan da biraz boşluk. sol üst köşenin konumu bu dikkat et. p2 için de aynısı geçerli
+
+assign player_hurtbox_w = PLAYER_W;
+
+assign player_hurtbox_h = PLAYER_H; //DİKKAT2: burada böyle yapınca hurtbox oyuncu biraz elevationa sahip olduğu için ekranın en altından başlayabilir. ama sorun değil orayla kimsenin işi yok nolacak. p2 için de aynısı geçerli
+
+endmodule
