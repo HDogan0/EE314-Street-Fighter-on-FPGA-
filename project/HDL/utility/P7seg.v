7288b44484303a4a5c7ba0e8a8f2a38b7a880b6b
kJe-gh <kljk.erten@gmail.com>
2026-06-06 11:20:25 +0300

    Update P7seg.v

--- project/HDL/utility/P7seg.v
+++ project/HDL/utility/P7seg.v
@@ -2,7 +2,7 @@ module P7seg (output reg [6:0] hexn,input [2:0] hex);
 	//P1 P2 kolay yazılsın diye
 	always @ (hex) begin
 			case (hex)
-				0 : hexn = 7'b0010000;
+				0 : hexn = 7'b0010100;
 				1 : hexn = 7'b1111001;
 				2 : hexn = 7'b0100100;
 			endcase
