cae4df51b8241694e51a4ad538990ea23a0972cf
kJe-gh <kljk.erten@gmail.com>
2026-06-17 15:17:11 +0300

    Update P7seg.v

--- project/HDL/utility/P7seg.v
+++ project/HDL/utility/P7seg.v
@@ -5,6 +5,9 @@ module P7seg (output reg [6:0] hexn,input [2:0] hex);
 				0 : hexn = 7'b0010100; //P
 				1 : hexn = 7'b1111001; //1
 				2 : hexn = 7'b0100100; //2
+				3 : hexn = 7'b1000001; //V
+				4 : hexn = 7'b0010010; //S
+				5 : hexn = 7'b0111111; //-
 			endcase
 	end
 
