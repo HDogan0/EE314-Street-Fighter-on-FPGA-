32a008140c9311254a1dcbdf9f86efce2307caba
HDogan0 <hsn.dogan04@gmail.com>
2026-06-18 23:23:34 +0300

    P düzeltildi

--- project/CodeGenerated/DE1_SOC/Top_Module/P7seg.v
+++ project/CodeGenerated/DE1_SOC/Top_Module/P7seg.v
@@ -2,7 +2,7 @@ module P7seg (output reg [6:0] hexn,input [2:0] hex);
 	//P1 P2 kolay yazılsın diye
 	always @ (hex) begin
 			case (hex)
-				0 : hexn = 7'b0010100; //P
+				0 : hexn = 7'b0001100; //P
 				1 : hexn = 7'b1111001; //1
 				2 : hexn = 7'b0100100; //2
 				3 : hexn = 7'b1000001; //V
